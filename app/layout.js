1fe33173aab6bba8e4d04efc3dbdfbd8b092da42
Lisle Abrahams <lisleabrahams.creative@gmail.com>
2026-01-28 10:28:10 +0100

    Add Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WDYSG2YNHD"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WDYSG2YNHD');
+            `,
+          }}
+        />
+      </head>
       <body style={{ margin: 0, padding: 0, fontFamily: 'Arial, sans-serif', overflowX: 'hidden' }}>
         <ScrollToTop />
         {children}
